40529916d8afbcbe845d6f4a7ca907dc67c95633
Nziza.jpg <tayalinziza@gmail.com>
2025-07-07 18:26:58 +0200

    done

---
+++ activity2.py
@@ -0,0 +1,21 @@
+# def sum(n):
+#     return n*(n+1)/2
+
+# print(sum(4))
+
+# def arraysum(a):
+#     sum=0
+#     for i in a:
+#         sum = sum + i
+
+#     return(sum)
+
+# a = [12, 3, 4, 15]
+# print(arraysum(a))
+
+def summ(n):
+    if(n<=0):
+        return
+    return n+ summ(n-1)
+
+print(summ(4))
